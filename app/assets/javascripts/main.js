695c0117ca6d7947c109f946876646daa0b05131
Gabe D <gpad88@gmail.com>
2016-05-14 16:16:20 -0700

    quick filter

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,6 +3,19 @@ window.setupHome = function(){
   $('.parallax').parallax();
   $('select').material_select();
   $('.modal-trigger').leanModal();
+  $('#search-city').on('keyup', function(){
+    var keyword = $(this).val().toLowerCase().replace(/ /g,'')
+    var patt = new RegExp(keyword);
+    $(".listing").each(function () {
+        var $this = $(this);
+        var city = $this.data('city').toLowerCase()
+        if (patt.test(city)) {
+            $this.show();
+        } else {
+            $this.hide();
+        }
+    });
+  })
 }
 
 
