66c0dfc6e617f3b7d072bcbe7e9690e1071cd8a7
Gabe D <gpad88@gmail.com>
2016-05-14 17:37:14 -0700

    migration git push

--- db/migrate/20160515002855_fix_price_col.rb
+++ db/migrate/20160515002855_fix_price_col.rb
@@ -1,7 +1,5 @@
 class FixPriceCol < ActiveRecord::Migration
   def change
-    remove_column :listings, :price_cents
-    remove_column :listings, :price_currency
     add_money :listings, :price
   end
 end
